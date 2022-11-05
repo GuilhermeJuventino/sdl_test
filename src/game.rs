12e6f495aad46a38462de04f7d0178940772f0e4
Guilherme Pereira Juventino <guilhermejuventino@gmail.com>
2022-11-04 23:11:38 -0300

    Added acceleration and deceleration to the ship

--- src/game.rs
+++ src/game.rs
@@ -1,18 +1,36 @@
 use specs::{World, WorldExt, Builder, Join};
 use std::collections::HashMap;
+use vector2d::Vector2D;
+
+use crate::{SCREEN_WIDTH, SCREEN_HEIGHT};
+use crate::utils::rad_to_deg;
 
 const ROTATION_SPEED: f64 = 1.5;
+const MAX_SPEED: f64 = 3.5;
+const ACCELERATION: f64 = 4.5;
+const DECELERATION: f64 = 0.99;
 
 pub fn update(ecs: &mut World, key_manager: &mut HashMap<String, bool>) {
     let mut positions = ecs.write_storage::<crate::components::Position>();
-    let players = ecs.read_storage::<crate::components::Player>();
+    let mut players = ecs.write_storage::<crate::components::Player>();
 
-    for (_, pos) in (&players, &mut positions).join() {
-        if crate::utils::is_key_pressed(&key_manager, "A") {
+    for (player, pos) in (&mut players, &mut positions).join() {
+        if crate::utils::is_key_pressed(&key_manager, "Left") {
             pos.rot -= ROTATION_SPEED;
         }
-        if crate::utils::is_key_pressed(&key_manager, "D") {
+        if crate::utils::is_key_pressed(&key_manager, "Right") {
             pos.rot += ROTATION_SPEED;
+        }
+
+        update_movement(pos, player);
+        if crate::utils::is_key_pressed(&key_manager, "Up") {
+            let radians = pos.rot.to_radians();
+            let move_x = radians.sin() * ACCELERATION;
+            let move_y = radians.cos() * ACCELERATION;
+
+            let move_vec = Vector2D::<f64>::new(move_x, move_y);
+
+            player.impulse += move_vec;
         }
 
         if pos.rot > 360.0 {
@@ -24,19 +42,40 @@ pub fn update(ecs: &mut World, key_manager: &mut HashMap<String, bool>) {
     }
 }
 
+pub fn update_movement(
+    pos: &mut crate::components::Position,
+    player: &mut crate::components::Player,
+) {
+    player.current_speed *= DECELERATION;
+
+    player.current_speed += player.impulse;
+    if player.current_speed.length() > MAX_SPEED {
+        player.current_speed = player.current_speed.normalise();
+        player.current_speed = player.current_speed * MAX_SPEED;
+    }
+
+    pos.x += player.current_speed.x;
+    pos.y -= player.current_speed.y;
+
+    player.impulse = Vector2D::new(0.0, 0.0);
+}
+
 pub fn load_world(ecs: &mut World) {
     ecs.create_entity()
-        .with(crate::components::Position{ x: 350.0, y: 250.0, rot: 0.0 })
+        .with(crate::components::Position{ x: SCREEN_WIDTH as f64 / 2.0, y: SCREEN_HEIGHT as f64 / 2.0, rot: 0.0 })
         .with(crate::components::Renderable{
             tex_name: String::from("img/space_ship.png"),
             i_w: 100,
             i_h: 100,
-            o_w: 100,
-            o_h: 100,
+            o_w: 50,
+            o_h: 50,
             frame: 0,
             total_frames: 1,
             rot: 0.0,
         })
-        .with(crate::components::Player{})
+        .with(crate::components::Player{
+            impulse: Vector2D::new(0.0, 0.0),
+            current_speed: Vector2D::new(0.0, 0.0),
+        })
         .build();
 }
